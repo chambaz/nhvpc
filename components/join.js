9bdef0589c2a8adf80e069835ee9c7befc743fdc
Adam Chambers <adammchambers@gmail.com>
2022-03-29 02:00:40 -0400

    Map background style in join panel

--- components/join.js
+++ components/join.js
@@ -1,13 +1,20 @@
 const Join = () => {
   return (
-    <div id="join" className="max-w-4xl mx-auto space-y-6">
-      <h2 className="text-4xl mb-8 font-bold font-serif my-6 text-black">
-        Buy a slice and join the club!
-      </h2>
-      <p className="text-lg font-medium">Coming to New Haven soon...</p>
-      <p className="text-8xl font-serif mb-5 bg-clip-text text-transparent bg-gradient-to-r from-fuchsia-500 to-indigo-500 font-normal">
-        5/1/2022
-      </p>
+    <div
+      id="join"
+      className="shadow-inner py-2 lg:py-16 lg:pb-20 leading-loose bg-gray-100 text-gray-500 text-center bg-no-repeat bg-cover"
+      style={{
+        backgroundImage: 'url(/img/map.jpg)',
+      }}>
+      <div className="max-w-4xl mx-auto space-y-6">
+        <h2 className="text-4xl mb-8 font-bold font-serif my-6 text-black">
+          Buy a slice and join the club!
+        </h2>
+        <p className="text-lg font-medium">Coming to New Haven soon...</p>
+        <p className="text-8xl font-serif mb-5 bg-clip-text text-gray-400 font-light">
+          5/1/2022
+        </p>
+      </div>
     </div>
   )
 }
