52762e9fc7d8b9d8da18b4d67776641b55e0f555
Adam Chambers <adammchambers@gmail.com>
2022-03-28 23:30:16 -0400

    Update about section title

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
           <div className="max-w-6xl mx-auto pt-16 leading-loose text-gray-500 text-center">
             <div id="about" className="max-w-4xl mx-auto mb-36 space-y-8">
               <h2 className="text-4xl font-bold font-serif my-6 text-black">
-                About NHVPC
+                New Haven Pizza Club
               </h2>
               <p className="max-w-2xl mx-auto">
                 New Haven Pizza Club is a members-only club for residents of New
